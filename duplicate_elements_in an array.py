d9ebfb2f8d2c14c35ec6468a3ac0d282ad2fc8fa
kriti-1-9 <dwivedikriti700@gmail.com>
2025-11-20 10:03:09 +0530

    updated duplicate elements in an array

--- duplicate_elements_in an array.py
+++ duplicate_elements_in an array.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     
 # [Better Approach - 1] Sorting with Consecutive Comparison - O(n × log n) Time and O(1) Space
 
-def findDuplicates(arr):
+"""def findDuplicates(arr):
     
     # sort the array so that duplicates are adjacent
     arr.sort()
@@ -75,4 +75,32 @@ def findDuplicates(arr):
 if __name__ == "__main__":
     arr = [2, 3, 1, 2, 3]
     res = findDuplicates(arr)
-    print(*res)
+    print(*res) """
+
+# [Better Approach - 2] Using Frequency Array - O(n) Time and O(n) Space
+
+def findDuplicates(arr):
+    
+    n = len(arr)
+    # frequency array with 1-based indexing
+    freq = [0] * (n + 1)  
+    ans = []
+
+    # count frequencies using the array
+    for num in arr:
+        freq[num] += 1
+
+    # collect elements that appear exactly twice
+    for i in range(1, n + 1):
+        if freq[i] == 2:
+            ans.append(i)
+
+    return ans
+
+if __name__ == "__main__":
+    arr = [2, 3, 1, 2, 3]
+    res = findDuplicates(arr)
+
+    for ele in res:
+        print(ele, end=' ')
+    print()
