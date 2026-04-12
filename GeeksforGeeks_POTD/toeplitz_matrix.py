51b540260e050b2eb0557fb477510c9bae282642
kriti-1-9 <dwivedikriti700@gmail.com>
2026-04-12 19:21:53 +0530

    added GFG POTD: Toeplitz MAtrix

---
+++ GeeksforGeeks_POTD/toeplitz_matrix.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isToeplitz(self, mat):
+        m, n = len(mat), len(mat[0])
+    
+        for i in range(1, m):
+            for j in range(1, n):
+                if mat[i][j] != mat[i-1][j-1]:
+                    return False
+
+        return True
