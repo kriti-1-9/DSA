ebb3d0603d1a6833323435eece7d624ced65b645
kriti-1-9 <dwivedikriti700@gmail.com>
2026-04-16 15:35:37 +0530

    Updated reverse array in gfg questions

--- Geeksforgeeks_questions/Arrays/reverse_arr.py
+++ Geeksforgeeks_questions/Arrays/reverse_arr.py
@@ -1,3 +1,96 @@
-class Solution:
-    def reverseArray(self, arr):
-        return arr.reverse()
+# Naive Approach
+
+"""
+def reverseArray(arr):
+    n = len(arr)
+    
+    # Temporary array to store elements
+    # in reversed order
+    temp = [0] * n
+  
+    # Copy elements from original array
+    # to temp in reverse order
+    for i in range(n):
+        temp[i] = arr[n - i - 1]
+  
+    # Copy elements back to original array
+    for i in range(n):
+        arr[i] = temp[i]
+
+if __name__ == "__main__":
+    arr = [1, 4, 3, 2, 6, 5]
+
+    reverseArray(arr)
+  
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+"""
+
+# expected approach 1
+
+"""
+def reverseArray(arr):
+    
+    # Initialize left to the beginning 
+    # and right to the end
+    left = 0
+    right = len(arr) - 1
+  
+    # Iterate till left is less than right
+    while left < right:
+        
+        # Swap the elements at left
+        # and right position
+        arr[left], arr[right] = arr[right], arr[left]
+      
+        # Increment the left pointer
+        left += 1
+      
+        # Decrement the right pointer
+        right -= 1
+
+if __name__ == "__main__":
+    arr = [1, 4, 3, 2, 6, 5]
+
+    reverseArray(arr)
+  
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+"""
+
+# expected approach 2
+
+"""
+def reverseArray(arr):
+    n = len(arr)
+    
+    # Iterate over the first half 
+    # and for every index i, swap
+    # arr[i] with arr[n - i - 1]
+    for i in range(n // 2):
+        temp = arr[i]
+        arr[i] = arr[n - i - 1]
+        arr[n - i - 1] = temp
+
+if __name__ == "__main__":
+    arr = [1, 4, 3, 2, 6, 5]
+
+    reverseArray(arr)
+  
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+"""
+
+# Using inbuilt Methods
+
+"""
+def reverseArray(arr):
+    arr.reverse()
+
+if __name__ == "__main__":
+    arr = [1, 4, 3, 2, 6, 5]
+
+    reverseArray(arr)
+  
+    print(" ".join(map(str, arr)))
+"""
