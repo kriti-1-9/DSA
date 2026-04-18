eaee641c18cfadb994647310b5975c0cd8e8eccc
kriti-1-9 <dwivedikriti700@gmail.com>
2026-04-18 14:26:11 +0530

    Added question Stock buy and sell - Max one Transaction allowed in GFG ques

---
+++ Geeksforgeeks_questions/Arrays/stock_but_and_sell_II.py
@@ -0,0 +1,7 @@
+def maxProfit(prices):
+    minSoFar = prices[0]
+    res = 0
+    for i in range(1, len(prices)):
+        minSoFar = min(minSoFar, prices[i])
+        res = max(res, prices[i] - minSoFar)
+    return res
