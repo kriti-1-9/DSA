f7d506a6c3393bf5ac65b1dcdc90365bf1d1261a
kriti-1-9 <dwivedikriti700@gmail.com>
2026-04-19 09:45:03 +0530

    Added question minimum number of operations to make an array palindrome in GFG ques

---
+++ Geeksforgeeks_questions/Arrays/min_num_of_operations_to_make_araay_palindrome.py
@@ -0,0 +1,16 @@
+def finMinOps(arr, n):
+    ans = 0
+    i, j = 0, n-1
+    while i <= j:
+        if arr[i] == arr[j]:
+            i += 1
+            j -= 1
+        elif arr[i] > arr[j]:
+            j -= 1
+            arr[j] += arr[j+1]
+            ans += 1
+        else:
+            i += 1
+            arr[i] += arr[i-1]
+            ans += 1
+    return ans
