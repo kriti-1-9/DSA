d366813134e2894dbe95b23d227f560943ea173d
kriti-1-9 <dwivedikriti700@gmail.com>
2026-06-18 22:53:16 +0530

    Added ques 14: Longest common prefix

---
+++ Leetcode/14_longest_common_prefix.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        strs.sort()
+        first = strs[0]
+        last = strs[-1]
+        minLength = min(len(first), len(last))
+        i = 0
+        while i < minLength and first[i] == last[i]:
+            i += 1
+        return first[:i]
