6a536b17bba7662f8d671a14d30242d2f29dc5af
kriti-1-9 <dwivedikriti700@gmail.com>
2026-08-11 18:27:55 +0530

    Added Leetcode question 165: Compare version numbers

---
+++ Leetcode/165_compare_version_numbers.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def compareVersion(self, version1, version2):
+        v1 = list(map(int, version1.split('.')))
+        v2 = list(map(int, version2.split('.')))
+    
+        max_len = max(len(v1), len(v2))
+        v1 += [0] * (max_len - len(v1))
+        v2 += [0] * (max_len - len(v2))
+    
+        for i in range(max_len):
+            if v1[i] < v2[i]:
+                return -1
+            elif v1[i] > v2[i]:
+                return 1
+        return 0       
