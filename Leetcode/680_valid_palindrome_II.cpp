da935c00f3840b10b971475ada6ce7343d1973e3
kriti-1-9 <dwivedikriti700@gmail.com>
2026-08-12 11:57:46 +0530

    Added Leetcode question 680: Valid palindrome II

---
+++ Leetcode/680_valid_palindrome_II.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string& s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    
+    bool validPalindrome(string s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s[left] != s[right]) {
+                // Try skipping either the left or the right character
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true; // If no mismatches, it's already a palindrome
+    }
+};
